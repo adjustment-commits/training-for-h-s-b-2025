6f02b8788d666da32eb2528c5d1d891ba70df42b
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-12-01 18:19:10 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,43 @@
 const CACHE_NAME = 'adjustment-lab-v1';
 const urlsToCache = [
-'./',
-'./index.html',
-'./extra/nutrition/index.html',
-'./extra/recovery/index.html',
-'./extra/thinking/index.html',
-'./images/icon_growth_nutrition.png',
-'./images/icon_recovery_lab.png',
-'./images/icon_think_lab.png'
+  './',
+  './index.html',
+  './extra/nutrition/index.html',
+  './extra/recovery/index.html',
+  './extra/thinking/index.html',
+  './images/icon_growth_nutrition.png',
+  './images/icon_recovery_lab.png',
+  './images/icon_think_lab.png'
 ];
 
 self.addEventListener('install', function(event) {
-event.waitUntil(
-caches.open(CACHE_NAME).then(function(cache) {
-return cache.addAll(urlsToCache);
-})
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-event.respondWith(
-caches.match(event.request).then(function(response) {
-return response || fetch(event.request);
-}).catch(function() {
-return caches.match('./index.html');
-})
-);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
-event.waitUntil(
-caches.keys().then(function(cacheNames) {
-return Promise.all(
-cacheNames.map(function(name) {
-if (name !== CACHE_NAME) {
-return caches.delete(name);
-}
-})
-);
-})
-);
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
 });
